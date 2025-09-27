9af0c08238eb70e5305c0bc75a994991d1c125ab
Dany Paredes <danywalls4@gmail.com>
2025-09-27 18:01:48 +0200

    Refactor: Replace Apollo GraphQL service with ProductsService in products component
    - Simplified data fetching by using `ProductsService` instead of Apollo GraphQL
    - Removed Apollo-specific setup and dependencies from `app.config.ts`

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,7 @@
 import {Component, inject} from '@angular/core';
-import {Product} from "../../services/products.service";
+import {ProductsService} from "../../services/products.service";
 import {AsyncPipe} from "@angular/common";
-import {Apollo} from "apollo-angular";
-import gql from "graphql-tag";
-import {map, tap} from "rxjs";
+
 import {RouterLink} from "@angular/router";
 
 @Component({
@@ -13,25 +11,7 @@ import {RouterLink} from "@angular/router";
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
-  private readonly apollo = inject(Apollo)
 
-  public products$ = this.apollo.watchQuery<{ products: Product[] }>(
-    {
-      query: gql`
-        {
-          products {
-            id,
-            price
-            image
-            title
-          }
-        }
-
-      `
-    }
-  ).valueChanges
-    .pipe(
-      tap(p => console.log(p)),
-      map(result => result.data.products)
-    )
+  productsService = inject(ProductsService)
+  public products$ = this.productsService.products$
 }
